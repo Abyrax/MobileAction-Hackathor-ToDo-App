b85f1d2f4355c065b6b065924a526446d6a35fc6
Abyrax <abyrax@gmail.com>
2022-12-16 23:50:11 +0300

    .

--- Todo App/View/SettingsView.swift
+++ Todo App/View/SettingsView.swift
@@ -26,59 +26,6 @@ struct SettingsView: View {
         // MARK: - FORM
         
         Form {
-          // MARK: - SECTION 1
-          
-          Section(header: Text("Choose the app icon")) {
-            Picker(selection: $iconSettings.currentIndex, label:
-              HStack {
-                ZStack {
-                  RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .strokeBorder(Color.primary, lineWidth: 2)
-                  
-                  Image(systemName: "paintbrush")
-                    .font(.system(size: 28, weight: .regular, design: .default))
-                    .foregroundColor(Color.primary)
-                }
-                .frame(width: 44, height: 44)
-                
-                Text("App Icons".uppercased())
-                  .fontWeight(.bold)
-                  .foregroundColor(Color.primary)
-              } //: LABEL
-              
-            ) {
-              ForEach(0..<iconSettings.iconNames.count) { index in
-                HStack {
-                  Image(uiImage: UIImage(named: self.iconSettings.iconNames[index] ?? "Blue") ?? UIImage())
-                    .renderingMode(.original)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 44, height: 44)
-                    .cornerRadius(8)
-                  
-                  Spacer().frame(width: 8)
-                  
-                  Text(self.iconSettings.iconNames[index] ?? "Blue")
-                    .frame(alignment: .leading)
-                } //: HSTACK
-                .padding(3)
-              }
-            } //: PICKER
-              .onReceive([self.iconSettings.currentIndex].publisher.first()) { (value) in
-                let index = self.iconSettings.iconNames.firstIndex(of: UIApplication.shared.alternateIconName) ?? 0
-                if index != value {
-                  UIApplication.shared.setAlternateIconName(self.iconSettings.iconNames[value]) { error in
-                    if let error = error {
-                      print(error.localizedDescription)
-                    } else {
-                      print("Success! You have changed the app icon.")
-                    }
-                  }
-                }
-            }
-          } //: SECTION 1
-            .padding(.vertical, 3)
-          
           // MARK: - SECTION 2
           
           Section(header:
@@ -116,38 +63,13 @@ struct SettingsView: View {
                 dismissButton: .default(Text("OK"))
               )
           }
-          
-          // MARK: - SECTION 3
-          
-          Section(header: Text("Follow us on social media")) {
-            FormRowLinkView(icon: "globe", color: Color.pink, text: "Website", link: "https://swiftuimasterclass.com")
-            FormRowLinkView(icon: "link", color: Color.blue, text: "Twitter", link: "https://twitter.com/robertpetras")
-            FormRowLinkView(icon: "play.rectangle", color: Color.green, text: "Courses", link: "https://www.udemy.com/user/robert-petras")
-          } //: SECTION 3
-            .padding(.vertical, 3)
-          
-          // MARK: - SECTION 4
-          
-          Section(header: Text("About the application")) {
-            FormRowStaticView(icon: "gear", firstText: "Application", secondText: "Todo")
-            FormRowStaticView(icon: "checkmark.seal", firstText: "Compatibility", secondText: "iPhone, iPad")
-            FormRowStaticView(icon: "keyboard", firstText: "Developer", secondText: "John / Jane")
-            FormRowStaticView(icon: "paintbrush", firstText: "Designer", secondText: "Robert Petras")
-            FormRowStaticView(icon: "flag", firstText: "Version", secondText: "1.5.0")
-          } //: SECTION 4
-            .padding(.vertical, 3)
         } //: FORM
           .listStyle(GroupedListStyle())
           .environment(\.horizontalSizeClass, .regular)
         
         // MARK: - FOOTER
         
-        Text("Copyright © All rights reserved.\nBetter Apps ♡ Less Code")
-          .multilineTextAlignment(.center)
-          .font(.footnote)
-          .padding(.top, 6)
-          .padding(.bottom, 8)
-          .foregroundColor(Color.secondary)
+        
       } //: VSTACK
         .navigationBarItems(trailing:
           Button(action: {
